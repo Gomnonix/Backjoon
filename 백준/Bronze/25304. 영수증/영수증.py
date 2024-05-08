f0e452c2465cc0f55d0c279df4cf78a7992b0c8e
Gomnonix <164334686+Gomnonix@users.noreply.github.com>
2024-05-08 20:41:36 +0900

    [Bronze IV] Title: 영수증, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.py
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -1,12 +1,12 @@
 X = int(input())
 N = int(input())
 
-sum = 0 
-for i in range(1, N+1):
-    a, b = map(int, input().split())
+sum = 0
+for i in range(0, N):
+    a,b = map(int, input().split())
     sum += a*b
 
-if sum == X:
+if X == sum:
     print("Yes")
 else:
     print("No")
