6c6d4b9402c074243a8b0652a30781af310c8cc8
Gomnonix <164334686+Gomnonix@users.noreply.github.com>
2024-05-30 19:57:30 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,10 @@
+ls_dict = {"ABC":3, "DEF":4, "GHI":5, "JKL":6, "MNO":7, "PQRS":8, "TUV":9, "WXYZ":10}
+
+s = input()
+time = 0
+
+for i in s:
+    for k,v in ls_dict.items():
+        if i in k:
+            time += v
+print(time)
